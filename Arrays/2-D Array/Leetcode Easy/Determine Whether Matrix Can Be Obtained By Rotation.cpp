6dbe9356240d156f0ccaacb9adfc6aca612852df
MAZHAR <iiestshibpur605@gmail.com>
2026-03-21 23:55:50 +0530

    Added YouTube Link

--- Arrays/2-D Array/Leetcode Easy/Determine Whether Matrix Can Be Obtained By Rotation.cpp
+++ Arrays/2-D Array/Leetcode Easy/Determine Whether Matrix Can Be Obtained By Rotation.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=N4KEX5-Pi34
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/
 */
