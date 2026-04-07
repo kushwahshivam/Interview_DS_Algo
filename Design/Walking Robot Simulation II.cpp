8ba00376e85c25433fc145476622c6a68ee31d4f
MAZHAR <iiestshibpur605@gmail.com>
2026-04-07 10:24:39 +0530

    Added YouTube Link

--- Design/Walking Robot Simulation II.cpp
+++ Design/Walking Robot Simulation II.cpp
@@ -1,6 +1,6 @@
 /*        Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO IN THIS QN : 
+    MY YOUTUBE VIDEO IN THIS QN : https://www.youtube.com/watch?v=YAGP17hvQjY
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/walking-robot-simulation-ii
 */
