b876c10176bbba0fcfc8517ff13622a591fe4cf6
MAZHAR <iiestshibpur605@gmail.com>
2026-04-08 06:31:17 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/XOR After Range Multiplication Queries I.cpp
+++ Arrays/Leetcode Easy/XOR After Range Multiplication Queries I.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=do3fgZMPiGs
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/xor-after-range-multiplication-queries-i
 */
