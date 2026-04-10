5ff55ab3e957fa8fa0a6a1bc4118a7f2959a1eee
MAZHAR <iiestshibpur605@gmail.com>
2026-04-10 21:14:09 +0530

    Added YouTube Link

--- HashMap/Minimum Distance Between Three Equal Elements I & II.cpp
+++ HashMap/Minimum Distance Between Three Equal Elements I & II.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=Qio1gMSEzG8
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i
                                   https://leetcode.com/problems/minimum-distance-between-three-equal-elements-ii
